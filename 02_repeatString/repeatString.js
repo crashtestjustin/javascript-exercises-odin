ab29068df72d74cf6162ece4d3f0b970f425a177
justin <thecrashtestjustin@gmail.com>
2022-09-22 07:48:23 -0700

    Completed the repeatString assignment

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(stringInut, numOfTimes) {
+    if (numOfTimes < 0){return 'ERROR';}
+    var stringOutput = '';
+    for (let i = 0; i < numOfTimes; i++) {
+        stringOutput += stringInut;
+    }    
+    return stringOutput;
 };
 
+repeatString ();
 // Do not edit below this line
 module.exports = repeatString;
