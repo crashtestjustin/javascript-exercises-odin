041cdf301dbd2138dfc227d961dae0bec75df8f5
justin <thecrashtestjustin@gmail.com>
2022-09-24 15:45:56 -0700

    completed the reverseString Assignmnet

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+// take string and split into an array of each character
+//starting with nth position of the array, combine the array into a string in reverse order
 
-};
+const reverseString = function(stringInput) {
+    // the 4 line code is what I originally came up with. The two lines below are single line simpler ways to output the code.
+    
+    let stringArray = Array.from(stringInput);
+    let reverseArray = stringArray.reverse();
+    let newString = reverseArray.join('');
+    return newString;
+
+    // return stringInput.split("").reverse().join("");
+    // return Array.from(stringInput).reverse().join("");
+    }
 
 // Do not edit below this line
 module.exports = reverseString;
