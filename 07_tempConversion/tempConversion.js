60bbfecb54cf1ab70298ae62fab2caa5ffb41605
justin <thecrashtestjustin@gmail.com>
2022-09-26 18:57:51 -0700

    Completed the tempConversion assignment

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(inputF) {
+  let celciusOut = (inputF - 32) * (5/9)
+  return Math.round(celciusOut * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(inputC) {
+  let farenHeitOut = (inputC * (9/5)) + 32;
+ return Math.round(farenHeitOut * 10) / 10;
 };
 
 // Do not edit below this line
