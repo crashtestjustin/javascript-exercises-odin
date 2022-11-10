66a741b4dce1b5a276a3314f206dbdfa6df942c9
justin <thecrashtestjustin@gmail.com>
2022-11-10 13:01:39 -0800

    completed the exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n > 0) {
+        let fib = [0,1];
+        let a = 0;
+        let b = 1;
+        let c = n;
+            for (let i=0; i < n; i++) {
+                newFibItem = fib[a] + fib[b];
+                fib.push(newFibItem);
+                a++;
+                b++;
+            }
+            return fib[n];
+        } else {
+            return "OOPS";
+        }
 };
 
 // Do not edit below this line
